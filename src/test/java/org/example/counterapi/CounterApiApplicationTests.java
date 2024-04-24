cb5ace12edfccbc08fd1cb54c6600702493d5b8d
AhmedF03 <[ahmedabdelbary44@gmail.com]>
2024-04-24 01:13:37 -0400

    First Commit

---
+++ src/test/java/org/example/counterapi/CounterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.counterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CounterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
